f1273aa06b518dc3bc0aa28f26c804e133634450
Brandon <b.mazey@nyu.edu>
2018-07-29 20:53:19 -0400

    ok SUPER SIMPLE

--- application.py
+++ application.py
@@ -1,23 +1,15 @@
-from flask import Flask, Blueprint
-from .restplus import api
-from .controllers import ns as sentence_namespace
+from flask import Flask
 
 
 application = Flask(__name__)
 
 
-def initialize_app(flask_app):
-    blueprint = Blueprint('api', __name__, url_prefix='/api')
-    api.init_app(blueprint)
-    api.add_namespace(sentence_namespace)
-    flask_app.register_blueprint(blueprint)
-
-    # db.init_app(flask_app)
-    # test
+@application.route("/")
+def hello():
+    return "Hello World!"
 
 
 def main():
-    initialize_app(application)
     application.debug = True
     application.run()
 
